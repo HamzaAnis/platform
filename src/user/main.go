710d9eb246ac0d56d697ac029868634d07e58bcd
hamzaanis <hamzaanis9514@gmail.com>
2024-03-15 00:05:26 +0500

    added http endpoints for the user handlers

--- src/user/main.go
+++ src/user/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"net"
+	"net/http"
 	"os"
+	"strings"
 
 	pb "github.com/HamzaAnis/platform/gen/user"
 	"github.com/HamzaAnis/platform/pkg/logger"
 	"github.com/HamzaAnis/platform/src/user/server"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
@@ -16,7 +22,8 @@ var (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":"+os.Getenv("SERVICE_PORT"))
+	addr := ":" + os.Getenv("SERVICE_PORT")
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -25,7 +32,26 @@ func main() {
 	serverImpl := server.NewUserServer()
 
 	pb.RegisterUserServer(s, serverImpl)
-	if err := s.Serve(lis); err != nil {
+
+	grpcMux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	// Register gRPC-Gateway with the ServeMux
+	err = pb.RegisterUserHandlerFromEndpoint(context.Background(), grpcMux, addr, opts)
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	httpServer := &http.Server{
+		Handler: h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+				s.ServeHTTP(w, r)
+			} else {
+				grpcMux.ServeHTTP(w, r)
+			}
+		}), &http2.Server{}),
+	}
+
+	if err := httpServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
